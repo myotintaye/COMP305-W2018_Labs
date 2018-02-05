bd2b8a30888c51eb43f429b88c1b0690c7bb7085
myotintaye <myotintaye00@gmail.com>
2018-02-04 19:30:48 -0500

    lab2

---
+++ Assets/Scripts/ButtonClickHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClickHandler : MonoBehaviour {
+
+
+    public GameObject boyGameObject;
+    //private Rigidbody2D rBody;
+    public Sprite boySprite;
+    private SpriteRenderer sRend;
+
+    // Use this for initialization
+    void Start () {
+       
+        sRend = boyGameObject.GetComponent<SpriteRenderer>();
+    }
+	void OnMouseDown()
+    {
+        //Debug.Log("Testing");
+        //rBody = boyGameObject.GetComponent<Rigidbody2D>();
+        sRend.sprite = boySprite;
+
+    }
+}
